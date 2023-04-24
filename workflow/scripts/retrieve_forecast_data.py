1830c29bea508da6e56696573a02aaae00f02021
ktehranchi <83722342+ktehranchi@users.noreply.github.com>
2023-04-24 12:57:27 -0700

    scripts to download and convert cec and ads data

---
+++ workflow/scripts/retrieve_forecast_data.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import glob, os
+from pathlib import Path
+# import urllib.request
+import logging
+import zipfile
+
+from pathlib import Path
+import os
+
+from _helpers import progress_retrieve, configure_logging
+
+
+logger = logging.getLogger(__name__)
+
+
+#configs and snakemake inputs
+path = '/Users/kamrantehranchi/Library/CloudStorage/OneDrive-Stanford/Kamran_OSW/WECC ADS PCM/ADS 2032/2032_ads_pcm_Public Data/Hourly Profiles in CSV format'
+data_year = 2032
+output_path = '/Users/kamrantehranchi/Local_Documents/pypsa-breakthroughenergy-usa/workflow/resources'
+
+
+
+def download_cec_forecasts(urls, path):
+    for key, url in urls.items():
+        save_path = Path(f"{path}/{key}.xlsx")
+        progress_retrieve(url, save_path)
+        csv_path = Path(f"{path}/{key}.csv")
+        pd.read_excel(save_path, sheet_name='Data', header=0, index_col=0).to_csv(csv_path)
+        os.remove(save_path)
+
+def preprocess_ads_data(profiletype, paths, output_path, year=data_year):
+    """
+    Preprocesses the load, solar, and wind data from the ADS PCM
+    :param profiletype: string, either 'load', 'solar', or 'wind'
+    :param paths: list of strings, paths to the files to be processed
+    :param output_path: string, path to the directory where the processed files will be saved
+    :return: None
+    """
+    df_combined = pd.DataFrame()
+    for i in range(len(paths)):
+        df = pd.read_csv(paths[i], header=0, index_col=0)
+        # df.columns = df.columns.str.split('_').str[1]
+        df.columns = df.columns.str.removeprefix(f'{profiletype}_')
+        df.columns = df.columns.str.removeprefix('PV_')
+        df.columns = df.columns.str.removeprefix('WT_')
+        
+        df.columns = df.columns.str.removesuffix('.dat')
+        df.columns = df.columns.str.removesuffix(f'_{year}')
+        df.columns = df.columns.str.removesuffix(f'_[18].dat: {year}')
+        df = df.iloc[1:8785, :]
+        df_combined = pd.concat([df_combined, df], axis=1)
+    df_combined.to_csv(os.path.join(output_path, 'ads_private_data', f'{profiletype}_{data_year}.csv'))
+    
+
+if __name__ == "__main__":
+    
+    logger = logging.getLogger(__name__)
+    rootpath = "../" #remove . for snakemake
+    # configure_logging(snakemake)
+
+    adspath = Path(f"{rootpath}/resources/ads_private_data")
+    adspath.mkdir(parents=True, exist_ok=True)
+    cecpath = Path(f"{rootpath}/resources/cec_forecasts")
+    cecpath.mkdir(parents=True, exist_ok=True)
+
+    #Download CEC forecasts
+    logger.info("Downloading CEC Forecasts")
+    cec_urls = {'pge':'https://efiling.energy.ca.gov/GetDocument.aspx?tn=248357', 'sce':'https://efiling.energy.ca.gov/GetDocument.aspx?tn=248355','sdge':'https://efiling.energy.ca.gov/GetDocument.aspx?tn=248353'}
+    download_cec_forecasts(cec_urls, cecpath)
+
+
+    #Process ASD PCM data
+    ads_filelist = []
+    for file in glob.glob(os.path.join(path,"*.csv")):
+        ads_filelist.append(os.path.join(path,file))
+
+    logger.info("Preprocessing ADS data")
+    preprocess_ads_data('Load', [s for s in ads_filelist if "Profile_Load" in s], output_path,data_year)
+    preprocess_ads_data('Solar', [s for s in ads_filelist if "Profile_Solar" in s], output_path,data_year)
+    preprocess_ads_data('wind', [s for s in ads_filelist if "Profile_Wind" in s], output_path,data_year)
+
+
+
+
+
