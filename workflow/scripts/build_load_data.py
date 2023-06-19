c0d2c93dfaa77d4fd53d98ffead8ffb568a13633
ktehranchi <83722342+ktehranchi@users.noreply.github.com>
2023-06-19 10:44:10 -0400

    minor changes

--- workflow/scripts/build_load_data.py
+++ workflow/scripts/build_load_data.py
@@ -7,48 +7,6 @@ import pandas as pd, glob, os, logging, pypsa
 from _helpers import progress_retrieve, configure_logging
 from add_electricity import load_costs, _add_missing_carriers_from_costs
 
-def process_ads_data(file_patterns):
-    for year, file_patterns_year in file_patterns.items():
-        ads_filelist = glob.glob(os.path.join(snakemake.input[f'ads_{year}'], '*.csv'))
-        for profile_type, pattern in file_patterns_year.items():
-            modify_ads_files(profile_type, [s for s in ads_filelist if pattern in s], year)
-
-def modify_ads_files(profiletype, paths, year):
-    """
-    Preprocesses the load, solar, and wind data from the ADS PCM
-    :param profiletype: string, either 'load', 'solar', or 'wind'
-    :param paths: list of strings, paths to the files to be processed
-    :param output_path: string, path to the directory where the processed files will be saved
-    :return: None
-    """
-    df_combined = pd.DataFrame()
-    # import pdb; pdb.set_trace()
-    for i in range(len(paths)):
-        df = pd.read_csv(paths[i], header=0, index_col=0,low_memory=False)
-        df = df.iloc[1:8785, :]
-        df_combined = pd.concat([df_combined, df], axis=1)
-    df_combined.to_csv(os.path.join("resources/WECC_ADS/processed", f'{profiletype}_{year}.csv'))
-
-    return None
-
-def preprocess_ads_load(df_ads,data_year):
-    df_ads['CISO-PGAE'] = df_ads.pop('CIPV') + df_ads.pop('CIPB') + df_ads.pop('SPPC')#hotfix see github issue #15
-    df_ads['BPAT'] = df_ads.pop('BPAT') + df_ads.pop('TPWR') + df_ads.pop('SCL')
-    df_ads['IPCO'] = df_ads.pop('IPFE') + df_ads.pop('IPMV') + df_ads.pop('IPTV')
-    df_ads['PACW'] = df_ads.pop('PAID') + df_ads.pop('PAUT') + df_ads.pop('PAWY')
-    df_ads['Arizona'] = df_ads.pop('SRP') + df_ads.pop('AZPS') 
-    df_ads.drop(columns=['Unnamed: 44', 'TH_Malin', 'TH_Mead', 'TH_PV'],inplace=True)
-    ba_list_map = {'CISC': 'CISO-SCE', 'CISD': 'CISO-SDGE','LDWP': 'LADWP','NWMT': 'MT_west','TIDC': 'TID','VEA': 'CISO-VEA','WAUW': 'WAUW_SPP'}
-    df_ads.rename(columns=ba_list_map,inplace=True)
-    df_ads['datetime'] = pd.Timestamp(f'{data_year}-01-01')+pd.to_timedelta(df_ads.index, unit='H')
-    df_ads.set_index('datetime',inplace=True)
-    if len(df_ads.index) > 8761: #remove leap year day
-        df_ads= df_ads[~(df_ads.index.date == pd.to_datetime(f'{data_year}-04-29'))]
-
-    # not_in_list = df_ads.loc[:,~df_ads.columns.isin(ba_list)]
-    return df_ads
-
-
 def add_breakthrough_demand_from_file(n, fn_demand):
 
     """
@@ -70,6 +28,40 @@ def add_breakthrough_demand_from_file(n, fn_demand):
 
     n.madd( "Load", demand_per_bus.columns, bus=demand_per_bus.columns, p_set=demand_per_bus)
     return n
+
+def process_ads_data(file_patterns):
+    for year, file_patterns_year in file_patterns.items():
+        ads_filelist = glob.glob(os.path.join(snakemake.input[f'ads_{year}'], '*.csv'))
+        for profile_type, pattern in file_patterns_year.items():
+            modify_ads_files(profile_type, [s for s in ads_filelist if pattern in s], year)
+
+def modify_ads_files(profiletype, paths, year):
+    df_combined = pd.DataFrame()
+    for i in range(len(paths)):
+        df = pd.read_csv(paths[i], header=0, index_col=0,low_memory=False)
+        df = df.iloc[1:8785, :]
+        df_combined = pd.concat([df_combined, df], axis=1)
+    df_combined.to_csv(os.path.join("resources/WECC_ADS/processed", f'{profiletype}_{year}.csv'))
+
+def preprocess_ads_load(df_ads,data_year):
+    df_ads['CISO-PGAE'] = df_ads.pop('CIPV') + df_ads.pop('CIPB') + df_ads.pop('SPPC')#hotfix see github issue #15
+    df_ads['BPAT'] = df_ads.pop('BPAT') + df_ads.pop('TPWR') + df_ads.pop('SCL')
+    df_ads['IPCO'] = df_ads.pop('IPFE') + df_ads.pop('IPMV') + df_ads.pop('IPTV')
+    df_ads['PACW'] = df_ads.pop('PAID') + df_ads.pop('PAUT') + df_ads.pop('PAWY')
+    df_ads['Arizona'] = df_ads.pop('SRP') + df_ads.pop('AZPS') 
+    df_ads.drop(columns=['Unnamed: 44', 'TH_Malin', 'TH_Mead', 'TH_PV'],inplace=True)
+    ba_list_map = {'CISC': 'CISO-SCE', 'CISD': 'CISO-SDGE','LDWP': 'LADWP','NWMT': 'MT_west','TIDC': 'TID','VEA': 'CISO-VEA','WAUW': 'WAUW_SPP'}
+    df_ads.rename(columns=ba_list_map,inplace=True)
+    df_ads['datetime'] = pd.Timestamp(f'{data_year}-01-01')+pd.to_timedelta(df_ads.index, unit='H')
+    df_ads.set_index('datetime',inplace=True)
+    if len(df_ads.index) > 8761: #remove leap year day
+        df_ads= df_ads[~(df_ads.index.date == pd.to_datetime(f'{data_year}-04-29'))]
+
+    # not_in_list = df_ads.loc[:,~df_ads.columns.isin(ba_list)]
+    return df_ads
+
+
+
 
 def add_ads_demand_from_file(n, fn_demand):
 
@@ -95,7 +87,6 @@ def add_ads_demand_from_file(n, fn_demand):
 
 
 if __name__ == "__main__":
-    
     logger = logging.getLogger(__name__)
     configure_logging(snakemake)
 
