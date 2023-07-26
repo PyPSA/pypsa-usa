Fixed Renewable Potential Calculation with update of the CRS projection for area calculation.

workflow/scripts/add_electricity.py
@@ -413,7 +413,9 @@ def attach_wind_and_solar(
             bus_profiles = ds["profile"].transpose("time", "bus").to_pandas().T.merge(bus2sub,left_on="bus", right_on="sub_id").set_index('bus_id').drop(columns='sub_id').T
             
             logger.info(f"Adding {car} capacity-factor profiles to the network.")
-            # import pdb; pdb.set_trace()
+            import pdb; pdb.set_trace()
+
+            #why is p_nom_max set to zero??
 
             n.madd(
                 "Generator",
@@ -513,7 +515,6 @@ def attach_conventional_generators(
             else:
                 # Single value affecting all generators of technology k indiscriminantely of country
                 n.generators.loc[idx, attr] = values
-
 
 def attach_hydro(n, costs, ppl, profile_hydro, hydro_capacities, carriers, **params):
     add_missing_carriers(n, carriers)
@@ -754,11 +755,11 @@ def attach_breakthrough_renewable_capacities_to_atlite(n, all_be_plants, renewab
         caps = tech_plants.groupby("bus_id").sum().Pmax #namplate capacity per bus
         # caps = caps / gens_per_bus.reindex(caps.index, fill_value=1) ##REVIEW do i need this
         #TODO: #16 Gens excluded from atlite profiles bc of landuse/etc will not be able to be attached if in the breakthrough network
-        # import pdb; pdb.set_trace()
 
         if caps[~caps.index.isin(network_gens.bus)].sum() > 0:
             missing_capacity = caps[~caps.index.isin(network_gens.bus)].sum()
             logger.info(f"There are {np.round(missing_capacity,1)} MW of {tech} plants that are not in the network. See git issue #16.")
+            import pdb; pdb.set_trace()
 
         n.generators.p_nom.update(network_gens.bus.map(caps).dropna())
         n.generators.p_nom_min.update(network_gens.bus.map(caps).dropna())