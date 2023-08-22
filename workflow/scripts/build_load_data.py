61b5c1102cc98286c1ed3f5caf066a2ffb290974
ktehranchi <83722342+ktehranchi@users.noreply.github.com>
2023-08-22 10:58:09 -0700

    Moved ADS file processing into the retrieve ads forecast data rule.

--- workflow/scripts/build_load_data.py
+++ workflow/scripts/build_load_data.py
@@ -27,19 +27,6 @@ def add_breakthrough_demand_from_file(n, fn_demand):
     n.madd( "Load", demand_per_bus.columns, bus=demand_per_bus.columns, p_set=demand_per_bus, carrier='AC')
     return n
 
-def prepare_ads_files(file_patterns):
-    for year, file_patterns_year in file_patterns.items():
-        ads_filelist = glob.glob(os.path.join(snakemake.input[f'ads_{year}'], '*.csv'))
-        for profile_type, pattern in file_patterns_year.items():
-            read_ads_files(profile_type, [s for s in ads_filelist if pattern in s], year)
-
-def read_ads_files(profiletype, paths, year):
-    df_combined = pd.DataFrame()
-    for i in range(len(paths)):
-        df = pd.read_csv(paths[i], header=0, index_col=0,low_memory=False)
-        df = df.iloc[1:8785, :]
-        df_combined = pd.concat([df_combined, df], axis=1)
-    df_combined.to_csv(os.path.join("resources/WECC_ADS/processed", f'{profiletype}_{year}.csv'))
 
 def prepare_ads_load_data(ads_load_path, data_year):
     '''
@@ -131,30 +118,7 @@ if __name__ == "__main__":
         n = add_eia_demand(n, eia_demand)
     elif snakemake.config['network_configuration']  == "ads2032":
         load_year = 2032
-        logger.info("Preproccessing ADS 2032 data")
-        os.makedirs("resources/WECC_ADS/processed/", exist_ok=True)
-        file_patterns = {   # Processed file name : Unprocessed file name
-                2032: {
-                    'load': 'Profile_Load',
-                    'solar': 'Profile_Solar',
-                    'wind': 'Profile_Wind',
-                    'hydro': 'Profile_Hydro',
-                    'btm_solar': 'Profile_BTM Solar',
-                    'pumped_storage': 'Profile_Pumped Storage',
-                    'pump_load': 'Profile_Pumps',
-                },
-                2030: {
-                    'load': 'Data_Load',
-                    'solar': 'Data_Solar PV',
-                    'wind': 'Data_WT',
-                    'hydro': 'Data_Hydro',
-                    'btm_solar': 'Data_SolarPV_Rooftop',
-                    'pumped_storage': 'Data_PumpStorage',
-                    'pump_load': 'Data_Pump',
-                }
-            }
-        prepare_ads_files(file_patterns)
-        demand = prepare_ads_load_data(f'resources/WECC_ADS/processed/load_{load_year}.csv',load_year)
+        demand = prepare_ads_load_data(f'data/WECC_ADS/processed/load_{load_year}.csv',load_year)
         n.set_snapshots(pd.date_range(freq="h", 
                                       start=f"{load_year}-01-01",
                                       end=f"{load_year+1}-01-01",
