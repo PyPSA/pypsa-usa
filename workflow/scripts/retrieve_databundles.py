01a54fd4efbc67c74249bcb2af2528271c87d1ec
ktehranchi <83722342+ktehranchi@users.noreply.github.com>
2023-10-18 10:55:35 -0700

    Fixes #100 #99

--- workflow/scripts/retrieve_databundles.py
+++ workflow/scripts/retrieve_databundles.py
@@ -25,12 +25,9 @@ if __name__ == "__main__":
         tarball_fn = Path(f"{rootpath}/{repository}.zip")
         to_fn = Path(f"{rootpath}/data/{subdir}")
 
-        if os.path.isfile(tarball_fn):
-            logger.info(f"{repository} zenodo repository already downloaded.")
-        else:
-            logger.info(f"Downloading {repository} zenodo repository from '{url}'.")
-            progress_retrieve(url, tarball_fn)
-        
+        logger.info(f"Downloading {repository} zenodo repository from '{url}'.")
+        progress_retrieve(url, tarball_fn)
+    
         logger.info(f"Extracting {repository} databundle.")
         with zipfile.ZipFile(tarball_fn, "r") as zip_ref:
             zip_ref.extractall(to_fn)
