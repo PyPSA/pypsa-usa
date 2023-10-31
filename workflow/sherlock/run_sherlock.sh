7cd6e8e22d3e9c60525de65d9458193023bf2a18
ktehranchi <83722342+ktehranchi@users.noreply.github.com>
2023-10-31 13:33:03 -0700

    sherlock

--- workflow/sherlock/run_sherlock.sh
+++ workflow/sherlock/run_sherlock.sh
@@ -5,3 +5,10 @@ ml gurobi/10.0.1_py39
 
 #Run Script
 snakemake -j6 --cluster config/config.cluster.yaml
+
+
+# snakemake -j6 --cluster "sbatch --time={resources.time_min} --mem={resources.mem_mb} --partition=serc --account=iazevedo -c {resources.cpus} -o logs/slurm/{rule}_{wildcards} -e logs/slurm/{rule}_{wildcards}"
+
+# snakemake -j6 --cluster "sbatch --partition=serc --account=iazevedo -c 1 -o logs/slurm/{rule}_{wildcards} -e logs/slurm/{rule}_{wildcards}"
+
+# snakemake -j6 --cluster "sbatch --time={resources.time_min} --partition=serc --account=iazevedo -c {resources.cpus} -o logs/slurm/{rule}_{wildcards} -e logs/slurm/{rule}_{wildcards}"
