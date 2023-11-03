2c158b00a976729d5113abd553e4a192f9ea3a06
trevorb1 <trevor_barnes@sfu.ca>
2023-11-03 12:03:23 -0700

    base capacity runs

--- workflow/scripts/plot_figures.py
+++ workflow/scripts/plot_figures.py
@@ -13,8 +13,10 @@ import logging
 from datetime import datetime
 from cartopy import crs as ccrs
 from pypsa.plot import add_legend_circles, add_legend_lines, add_legend_patches
-import logging
 
+import logging
+logger = logging.getLogger(__name__)
+from _helpers import configure_logging
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -30,19 +32,170 @@ import cartopy.crs as ccrs
 # Global Plotting Settings
 TITLE_SIZE = 16
 
-def plot_capacity_map(n, bus_values, generating_link_carrier_map, regions_onshore, n_clusters):
-    '''
-    Plot the map of network where each node is a pie chart defined by the input of bus_values
-    '''
-    if snakemake.wildcards.interconnect != "usa":
-        bus_scale = 1e5
-    else:
-        bus_scale = 4e4
-    if snakemake.wildcards.interconnect != "usa":
-        line_scale = 2e3
-    else:
-        line_scale = 4e3
 
+# def create_area_plot(production, demand, colors, snapshots, output_path, timeslice):
+#     fig, ax = plt.subplots(figsize=(14, 4))
+#     production[snapshots].plot.area(ax=ax, color=colors, alpha=0.7, legend="reverse")
+#     ax.legend(bbox_to_anchor=(1, 1), loc="upper left")
+#     ax.set_ylabel("Power [GW]")
+#     ax.set_xlabel("")
+#     fig.tight_layout()
+#     suffix = (
+#         "-" + datetime.strptime(str(timeslice), "%m").strftime("%b")
+#         if timeslice != "all"
+#         else ""
+#     )
+#     fig.savefig(output_path.parent / (output_path.stem + suffix + output_path.suffix))
+
+
+# def create_total_production_bar(total_production, colors, output_path):
+#     fig, ax = plt.subplots()
+#     total_production.div(1e3).plot.bar(color=colors, ax=ax)
+#     ax.set_ylabel("Total production [TWh]")
+#     ax.set_xlabel("")
+#     fig.savefig(output_path)
+
+
+# def create_cost_bar(operational_costs, capital_costs, colors, output_path):
+#     fig, ax = plt.subplots()
+
+#     costs = pd.concat([operational_costs, capital_costs], axis=1, keys=["OPEX", "CAPEX"])
+#     costs = costs.reset_index("carrier")
+
+#     s1 = sns.barplot(y="carrier", x="CAPEX", data=costs, alpha=0.6, ax=ax, palette=colors)
+#     s2 = sns.barplot(y="carrier", x="OPEX", data=costs, ax=ax, left=costs["CAPEX"], palette=colors)
+
+#     ax.set_ylabel("")
+#     ax.set_xlabel("CAPEX & OPEX [$]")
+#     fig.savefig(output_path)
+
+# def main(snakemake):
+
+#     os.chdir(os.getcwd())
+#     rootpath = "."
+
+#     #Import Files
+#     n = pypsa.Network(snakemake.input.network)
+#     regions_onshore = gpd.read_file(snakemake.input.regions_onshore)
+    
+#     #Parameters & Wildcards
+#     n_clusters = snakemake.wildcards.clusters
+#     ll = snakemake.wildcards.ll
+#     opts = snakemake.wildcards.opts
+#     n_hours = snakemake.config['solving']['options']['nhours']
+
+#     #Plotting Settings
+#     sns.set_theme("paper", style="darkgrid")
+
+#     generating_link_carrier_map = {"fuel cell": "H2", "battery discharger": "battery"}
+
+#     #Base Capacity Map 
+#     gen_pnom = n.generators.groupby(["bus", "carrier"]).p_nom.sum()
+#     storage_capacity = (n.storage_units.groupby(["bus", "carrier"]).p_nom.sum())
+#     bus_base_capacities = pd.concat([gen_pnom, storage_capacity])
+#     fig, ax = plot_capacity_map(n, bus_base_capacities, generating_link_carrier_map, regions_onshore, n_clusters)
+#     fig.savefig(snakemake.output[0])
+
+#     #Optimized Capacity Map
+#     gen_pnom_opt = n.generators.groupby(["bus", "carrier"]).p_nom_opt.sum()
+#     storage_capacity_opt = (n.storage_units.groupby(["bus", "carrier"]).p_nom_opt.sum())
+#     bus_opt_capacities = pd.concat([gen_pnom_opt, storage_capacity_opt])
+#     fig, ax = plot_capacity_map(n, bus_opt_capacities, generating_link_carrier_map, regions_onshore, n_clusters)
+#     fig.savefig(snakemake.output[1])
+
+#     #Built Capacity Map
+#     gen_built = gen_pnom_opt - gen_pnom
+#     storage_built = storage_capacity_opt - storage_capacity
+#     buses_built_capacities = pd.concat([gen_built, storage_built])
+#     fig, ax = plot_capacity_map(n, buses_built_capacities, generating_link_carrier_map, regions_onshore, n_clusters)
+#     fig.savefig(snakemake.output[2])
+
+
+#     carriers_gen = n.generators.carrier
+#     carriers_storageUnits = n.storage_units.carrier
+#     production = n.generators_t.p.groupby(carriers_gen, axis=1).sum().rename(columns=n.carriers.nice_name)/ 1e3
+#     # storage = network.storage_units_t.p.groupby(carriers_storageUnits, axis=1).sum()/1e3
+#     # storage_charge = storage[storage > 0].fillna(0).rename(columns={'battery':'Battery Discharging'})
+#     # storage_discharge = storage[storage < 0].fillna(0).rename(columns={'battery':'Battery Charging'})
+#     # energymix = pd.concat([production, storage_charge, storage_discharge], axis=1)
+#     demand = n.loads_t.p.sum(1).rename("Demand") / 1e3
+
+#     if n_hours < 8760:
+#         enddate = pd.to_datetime(snakemake.config["snapshots"]["end"])
+#         year = enddate.year
+#         for timeslice in list(range(1, enddate.month)) + ["all"]:
+#             snapshots = (
+#                 n.snapshots.get_loc(f"{year}-{timeslice}")
+#                 if timeslice != "all"
+#                 else slice(None, None)
+#             )
+#             create_area_plot(production, demand, colors, snapshots, snakemake.output.operation_area, timeslice)
+#     else:
+#         for timeslice in list(range(1, 12)) + ["all"]:
+#             snapshots = (
+#                 n.snapshots.get_loc(f"{year}-{timeslice}")
+#                 if timeslice != "all"
+#                 else slice(None, None)
+#             )
+#             create_area_plot(production, demand, colors, snapshots, snakemake.output.operation_area, timeslice)
+
+#     create_total_production_bar(total_production, colors, snakemake.output.operation_bar)
+
+#     operational_costs = (
+#         (production * n.generators.marginal_cost)
+#         .groupby(carriers, axis=1)
+#         .sum()
+#         .rename(columns=n.carriers.nice_name)
+#     ).sum()
+
+#     capital_costs = (
+#         n.generators.eval("p_nom_opt * capital_cost")
+#         .groupby(carriers)
+#         .sum()
+#         .rename(n.carriers.nice_name)
+#     )
+
+#     create_cost_bar(operational_costs, capital_costs, colors, snakemake.output.cost_bar)
+
+def get_bus_scale(interconnect: str) -> float:
+    if interconnect != "usa":
+        return 1e5
+    else:
+        return 4e4
+    
+def get_line_scale(interconnect: str) -> float:
+    if interconnect != "usa":
+        return 2e3
+    else:
+        return 4e3
+
+def create_title(title: str, **wildcards) -> str:
+    """Standardizes wildcard writing in titles
+    
+    Arguments: 
+        title: str
+            Title of chart to plot
+        **wildcards
+            any wildcards to add to title 
+    """
+    w = []
+    for wildcard, value in wildcards.items():
+        if wildcard == "interconnect":
+            w.append(f"interconnect = {value}")
+        elif wildcard == "clusters":
+            w.append(f"#clusters = {value}")
+        elif wildcard == "ll":
+            w.append(f"ll = {value}")
+        elif wildcard == "opts":
+            w.append(f"opts = {value}")
+    wildcards_joined = " | ".join(w)
+    return f"{title} ({wildcards_joined})"
+        
+
+def plot_capacity_map(n: pypsa.Network, bus_values: pd.DataFrame, regions: gpd.GeoDataFrame, colors=None, bus_scale=1, line_scale=1, title = None) -> (plt.figure, plt.axes):
+    """
+    Generic network plotting function for capacity pie charts at each node
+    """
 
     fig, ax = plt.subplots(
         figsize=(8, 8), subplot_kw={"projection": ccrs.EqualEarth(n.buses.x.mean())}
@@ -59,8 +212,9 @@ def plot_capacity_map(n, bus_values, generating_link_carrier_map, regions_onshor
             margin=0.2,
             color_geomap=None,
         )
-
-    regions_onshore.plot(
+        
+    # onshore regions
+    regions.plot(
         ax=ax,
         facecolor="whitesmoke",
         edgecolor="white",
@@ -68,7 +222,7 @@ def plot_capacity_map(n, bus_values, generating_link_carrier_map, regions_onshor
         transform=ccrs.PlateCarree(),
         linewidth=1.2,
     )
-    ax.set_extent(regions_onshore.total_bounds[[0, 2, 1, 3]])
+    ax.set_extent(regions.total_bounds[[0, 2, 1, 3]])
 
     legend_kwargs = {"loc": "upper left", "frameon": False}
     bus_sizes = [5000, 10e3, 50e3]  # in MW
@@ -92,146 +246,61 @@ def plot_capacity_map(n, bus_values, generating_link_carrier_map, regions_onshor
         n.carriers.nice_name,
         legend_kw={"bbox_to_anchor": (1, 0), **legend_kwargs, "loc": "lower left"},
     )
-    ax.set_title(f'Base Network Capacities  (#clusters = {n_clusters})', fontsize=TITLE_SIZE)
+    if not title:   
+        ax.set_title(f"Capacity (MW)", fontsize=TITLE_SIZE)
+    else:
+        ax.set_title(title, fontsize=TITLE_SIZE)
     fig.tight_layout()
     
     return fig, ax
 
-
-def create_area_plot(production, demand, colors, snapshots, output_path, timeslice):
-    fig, ax = plt.subplots(figsize=(14, 4))
-    production[snapshots].plot.area(ax=ax, color=colors, alpha=0.7, legend="reverse")
-    ax.legend(bbox_to_anchor=(1, 1), loc="upper left")
-    ax.set_ylabel("Power [GW]")
-    ax.set_xlabel("")
-    fig.tight_layout()
-    suffix = (
-        "-" + datetime.strptime(str(timeslice), "%m").strftime("%b")
-        if timeslice != "all"
-        else ""
-    )
-    fig.savefig(output_path.parent / (output_path.stem + suffix + output_path.suffix))
-
-
-def create_total_production_bar(total_production, colors, output_path):
-    fig, ax = plt.subplots()
-    total_production.div(1e3).plot.bar(color=colors, ax=ax)
-    ax.set_ylabel("Total production [TWh]")
-    ax.set_xlabel("")
-    fig.savefig(output_path)
-
-
-def create_cost_bar(operational_costs, capital_costs, colors, output_path):
-    fig, ax = plt.subplots()
-
-    costs = pd.concat([operational_costs, capital_costs], axis=1, keys=["OPEX", "CAPEX"])
-    costs = costs.reset_index("carrier")
-
-    s1 = sns.barplot(y="carrier", x="CAPEX", data=costs, alpha=0.6, ax=ax, palette=colors)
-    s2 = sns.barplot(y="carrier", x="OPEX", data=costs, ax=ax, left=costs["CAPEX"], palette=colors)
-
-    ax.set_ylabel("")
-    ax.set_xlabel("CAPEX & OPEX [$]")
-    fig.savefig(output_path)
-
-def main(snakemake):
-    logger = logging.getLogger(__name__)
-
-    os.chdir(os.getcwd())
-    rootpath = "."
-
-    #Import Files
-    n = pypsa.Network(snakemake.input.network)
-    regions_onshore = gpd.read_file(snakemake.input.regions_onshore)
-    
-    #Parameters & Wildcards
-    n_clusters = snakemake.wildcards.clusters
-    ll = snakemake.wildcards.ll
-    opts = snakemake.wildcards.opts
-    n_hours = snakemake.config['solving']['options']['nhours']
-
-    #Plotting Settings
-    sns.set_theme("paper", style="darkgrid")
-
-    generating_link_carrier_map = {"fuel cell": "H2", "battery discharger": "battery"}
-
-    #Base Capacity Map 
+def plot_base_capacity(n: pypsa.Network, regions: gpd.GeoDataFrame, colors = None, **wildcards) -> (plt.figure, plt.axes):
     gen_pnom = n.generators.groupby(["bus", "carrier"]).p_nom.sum()
-    storage_capacity = (n.storage_units.groupby(["bus", "carrier"]).p_nom.sum())
-    bus_base_capacities = pd.concat([gen_pnom, storage_capacity])
-    fig, ax = plot_capacity_map(n, bus_base_capacities, generating_link_carrier_map, regions_onshore, n_clusters)
-    fig.savefig(snakemake.output[0])
-
-    #Optimized Capacity Map
-    gen_pnom_opt = n.generators.groupby(["bus", "carrier"]).p_nom_opt.sum()
-    storage_capacity_opt = (n.storage_units.groupby(["bus", "carrier"]).p_nom_opt.sum())
-    bus_opt_capacities = pd.concat([gen_pnom_opt, storage_capacity_opt])
-    fig, ax = plot_capacity_map(n, bus_opt_capacities, generating_link_carrier_map, regions_onshore, n_clusters)
-    fig.savefig(snakemake.output[1])
-
-    #Built Capacity Map
-    gen_built = gen_pnom_opt - gen_pnom
-    storage_built = storage_capacity_opt - storage_capacity
-    buses_built_capacities = pd.concat([gen_built, storage_built])
-    fig, ax = plot_capacity_map(n, buses_built_capacities, generating_link_carrier_map, regions_onshore, n_clusters)
-    fig.savefig(snakemake.output[2])
-
-
-    carriers_gen = n.generators.carrier
-    carriers_storageUnits = n.storage_units.carrier
-    production = n.generators_t.p.groupby(carriers_gen, axis=1).sum().rename(columns=n.carriers.nice_name)/ 1e3
-    # storage = network.storage_units_t.p.groupby(carriers_storageUnits, axis=1).sum()/1e3
-    # storage_charge = storage[storage > 0].fillna(0).rename(columns={'battery':'Battery Discharging'})
-    # storage_discharge = storage[storage < 0].fillna(0).rename(columns={'battery':'Battery Charging'})
-    # energymix = pd.concat([production, storage_charge, storage_discharge], axis=1)
-    demand = n.loads_t.p.sum(1).rename("Demand") / 1e3
-
-    if n_hours < 8760:
-        enddate = pd.to_datetime(snakemake.config["snapshots"]["end"])
-        year = enddate.year
-        for timeslice in list(range(1, enddate.month)) + ["all"]:
-            snapshots = (
-                n.snapshots.get_loc(f"{year}-{timeslice}")
-                if timeslice != "all"
-                else slice(None, None)
-            )
-            create_area_plot(production, demand, colors, snapshots, snakemake.output.operation_area, timeslice)
-    else:
-        for timeslice in list(range(1, 12)) + ["all"]:
-            snapshots = (
-                n.snapshots.get_loc(f"{year}-{timeslice}")
-                if timeslice != "all"
-                else slice(None, None)
-            )
-            create_area_plot(production, demand, colors, snapshots, snakemake.output.operation_area, timeslice)
-
-    create_total_production_bar(total_production, colors, snakemake.output.operation_bar)
-
-    operational_costs = (
-        (production * n.generators.marginal_cost)
-        .groupby(carriers, axis=1)
-        .sum()
-        .rename(columns=n.carriers.nice_name)
-    ).sum()
-
-    capital_costs = (
-        n.generators.eval("p_nom_opt * capital_cost")
-        .groupby(carriers)
-        .sum()
-        .rename(n.carriers.nice_name)
+    storage_pnom = n.storage_units.groupby(["bus", "carrier"]).p_nom.sum()
+    bus_pnom = pd.concat([gen_pnom, storage_pnom])
+    title = create_title("Base Network Capacities", **wildcards)
+    
+    interconnect = wildcards.get("interconnect", None)
+    bus_scale = get_bus_scale(interconnect=wildcards["interconnect"]) if interconnect else 1
+    line_scale = get_line_scale(interconnect=wildcards["interconnect"]) if interconnect else 1
+    
+    return plot_capacity_map(
+        n=n, 
+        bus_values=bus_pnom,
+        regions=regions,
+        line_scale=line_scale,
+        bus_scale=bus_scale,
+        title=title
     )
-
-    create_cost_bar(operational_costs, capital_costs, colors, snakemake.output.cost_bar)
-
-
 
 if __name__ == "__main__":
     if 'snakemake' not in globals():
         from _helpers import mock_snakemake
-        snakemake = mock_snakemake('plot_figures', 
-                                    interconnect='western',
-                                    clusters=60,
-                                    ll='vopt',
-                                    opts='[Co2L0.75]',
-                                    )
-    main(snakemake)
+        snakemake = mock_snakemake(
+            'plot_figures', 
+            interconnect='western',
+            clusters=60,
+            ll='vopt',
+            opts='Co2L0.75',
+        )
+    configure_logging(snakemake)
+    # logging.basicConfig(level=snakemake.config["logging"]["level"])
+    
+    # extract shared plotting files 
+    n = pypsa.Network(snakemake.input.network)
+    onshore_regions = gpd.read_file(snakemake.input.regions_onshore)
+    colors = snakemake.params.plotting
+    n_clusters = snakemake.wildcards.clusters
+    ll = snakemake.wildcards.ll
+    opts = snakemake.wildcards.opts
+    n_hours = snakemake.config['solving']['options']['nhours']
+    
+    # mappers 
+    generating_link_carrier_map = {"fuel cell": "H2", "battery discharger": "battery"}
+    
+    # plotting theme
+    sns.set_theme("paper", style="darkgrid")
+    
+    # create plots
+    fig, _ = plot_base_capacity(n, onshore_regions, **snakemake.wildcards)
+    fig.savefig(snakemake.output["capacity_map_base"])
