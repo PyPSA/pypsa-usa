c345157a4f3e6e47d71e23a620c194486e76f7ba
Kamran <ktehranchi@stanford.edu>
2024-01-15 15:31:48 -0800

    run slurm script dialed in

--- workflow/run_slurm.sh
+++ workflow/run_slurm.sh
@@ -1,17 +1,2 @@
-# #!/bin/bash
-# #SBATCH -t 01:00:00
-# #SBATCH -p serc
-# #SBATCH -N 1
-# #SBATCH -cpus-per-task=1
-# #SBATCH --output log/snake-%j.out
-
-# # go to a particular directory
-# cd $GROUP_HOME/kamran/pypsa-usa/workflow 
-
-# # make things fail on errors
-# set -o nounset
-# set -o errexit
-# set -x
-
-### run your commands here!
-snakemake --cluster "sbatch -A {cluster.account} -p {cluster.partition} -t {cluster.time} -o {cluster.output} -e {cluster.error}" --cluster-config config/config.cluster.yaml --jobs 8 --latency-wait 10
+# SLURM specifications made in default.cluster.yaml & the individual rules
+snakemake --cluster "sbatch -A {cluster.account} -p {cluster.partition} -t {cluster.time} -o {cluster.output} -e {cluster.error} --mem {resources.mem_mb}" --cluster-config config/config.cluster.yaml --jobs 9 --latency-wait 10
