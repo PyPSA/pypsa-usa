22b40a5e56509ff1781758fce443ae171aeb48ff
trevorb1 <trevor_barnes@sfu.ca>
2024-04-28 12:19:07 -0700

    update loads dashboard

--- workflow/scripts/plot_loads.py
+++ workflow/scripts/plot_loads.py
@@ -40,7 +40,9 @@ RADIO_BUTTON_RESAMPLE = "radio_button_resample"
 RADIO_BUTTON_LOAD = "radio_button_load"
 
 # graphics
+GRAPHIC_CONTAINER = "graphic_container"
 GRAPHIC_MAP = "graphic_map"
+GRAPHIC_TIMESERIES = "graphic_timeseries"
 
 # tabs
 
@@ -125,20 +127,24 @@ def get_load_names(n: pypsa.Network, carriers: list[str]) -> list[str]:
 
 def get_load_timeseries(n: pypsa.Network, loads: list[str]) -> pd.DataFrame:
     """
-    Gets timeseries of loads resampled to days.
+    Gets timeseries of select carriers.
     """
     df = n.loads_t.p_set[loads]
     df.index = pd.to_datetime(df.index)
-    df = df.resample("24h").sum()
     return df
 
 
+def resample_load(df: pd.DataFrame) -> pd.DataFrame:
+    return df.resample("24h").sum()
+
+
 def filter_on_time(df: pd.DataFrame, doy: pd.Timestamp) -> pd.Series:
-    date = datetime(2019, 1, 1) + timedelta(doy - 1)
-    return df.loc[date]
+    day = datetime(2019, 1, 1) + timedelta(doy - 1)
+    assert isinstance(df.index, pd.DatetimeIndex)
+    return df[df.index.date == day.date()]
 
 
-def group_by_carrier(n: pypsa.Network, df: pd.Series) -> pd.Series:
+def group_sum_by_carrier(n: pypsa.Network, df: pd.Series) -> pd.Series:
     """
     Groups carriers to bus.
     """
@@ -148,6 +154,20 @@ def group_by_carrier(n: pypsa.Network, df: pd.Series) -> pd.Series:
     loads["region"] = loads.Load.map(n.loads.bus)
     loads = loads.drop(columns="Load")
     return loads.groupby("region").sum()
+
+
+def group_by_bus(n: pypsa.Network, df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Groups carriers to bus.
+    """
+    return df.rename(columns=n.loads.bus).T.groupby(level=0).sum().T
+
+
+def format_timeseries(df: pd.DataFrame) -> pd.DataFrame:
+    load = df.melt(ignore_index=False).reset_index()
+    load["hour"] = load.snapshot.map(lambda x: x.hour)
+    load = load.rename(columns={"Load": "region"})
+    return load[["region", "value", "hour"]]
 
 
 ###
@@ -238,7 +258,48 @@ def plot_load(
     load_names = get_load_names(n, carriers)
     loads = get_load_timeseries(n, load_names)
     loads = filter_on_time(loads, doy)
-    loads = group_by_carrier(n, loads)
+
+    return html.Div(
+        [
+            html.Div(
+                [plot_map(n, shapes, loads)],
+                style={
+                    "width": "48%",
+                    "display": "inline-block",
+                    "padding": "5px",
+                },
+            ),
+            html.Div(
+                [plot_timeseries(n, loads)],
+                style={
+                    "width": "48%",
+                    "display": "inline-block",
+                    "padding": "5px",
+                },
+            ),
+        ],
+        id=GRAPHIC_CONTAINER,
+    )
+
+
+@app.callback(
+    Output(GRAPHIC_CONTAINER, "children"),
+    Input(DROPDOWN_SELECT_SECTOR, "value"),
+    Input(DROPDOWN_SELECT_FUEL, "value"),
+    Input(SLIDER_SELECT_TIME, "value"),
+)
+def plot_load_callback(
+    sectors: list[str] = SECTORS,
+    fuels: list[str] = FUELS,
+    doy: pd.DatetimeIndex = NETWORK.snapshots.min().timetuple().tm_yday,
+) -> html.Div:
+    return plot_load(NETWORK, SHAPES, sectors, fuels, doy)
+
+
+def plot_map(n: pypsa.Network, shapes: gpd.GeoDataFrame, df: pd.DataFrame) -> html.Div:
+
+    loads = df.sum()  # get daily total
+    loads = group_sum_by_carrier(n, loads)
 
     gdf = shapes.join(loads)
 
@@ -254,25 +315,28 @@ def plot_load(
     title = "Load per Modelled Region"
     fig.update_layout(
         title=dict(text=title, font=dict(size=24)),
-        margin={"r": 0, "t": 0, "l": 0, "b": 0},
+        # margin={"r": 0, "t": 0, "l": 0, "b": 0},
     )
 
     return html.Div(children=[dcc.Graph(figure=fig)], id=GRAPHIC_MAP)
 
 
-@app.callback(
-    Output(GRAPHIC_MAP, "children"),
-    Input(DROPDOWN_SELECT_SECTOR, "value"),
-    Input(DROPDOWN_SELECT_FUEL, "value"),
-    Input(SLIDER_SELECT_TIME, "value"),
-)
-def plot_load_callback(
-    sectors: list[str] = SECTORS,
-    fuels: list[str] = FUELS,
-    doy: pd.DatetimeIndex = NETWORK.snapshots.min().timetuple().tm_yday,
+def plot_timeseries(
+    n: pypsa.Network,
+    df: pd.DataFrame,
 ) -> html.Div:
 
-    return plot_load(NETWORK, SHAPES, sectors, fuels, doy)
+    load = group_by_bus(n, df)
+    load = format_timeseries(load)
+
+    fig = px.line(load, x="hour", y="value", color="region")
+
+    title = "Loads [GW]"
+    fig.update_layout(
+        title=dict(text=title, font=dict(size=24)),
+    )
+
+    return html.Div(children=[dcc.Graph(figure=fig)], id=GRAPHIC_TIMESERIES)
 
 
 ###
