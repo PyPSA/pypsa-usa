41993b715f7596187705afee3604c5b2fcea75fe
ktehranchi <83722342+ktehranchi@users.noreply.github.com>
2024-06-18 10:09:28 -0700

    ..

--- workflow/scripts/build_powerplants.py
+++ workflow/scripts/build_powerplants.py
@@ -846,8 +846,8 @@ if __name__ == "__main__":
     # temp throwing out plants without
     missing_locations = plants[plants.longitude.isna() | plants.latitude.isna()]
     print('Tossing out plants without locations:', missing_locations.shape[0])
-    plants[plants.index.isin(missing_locations.index)].to_csv('missing_gps_pudl.csv')
+    # plants[plants.index.isin(missing_locations.index)].to_csv('missing_gps_pudl.csv')
     plants = plants[~plants.index.isin(missing_locations.index)]
-    print(plants)
+    # print(plants)
 
     plants.to_csv(snakemake.output.powerplants, index=False)
