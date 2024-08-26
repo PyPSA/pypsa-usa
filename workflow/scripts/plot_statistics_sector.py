05f9bb2c3394c04278204826d8d3a706e104bb57
trevorb1 <trevor_barnes@sfu.ca>
2024-08-25 20:29:35 -0700

    urban rural split

--- workflow/scripts/plot_statistics_sector.py
+++ workflow/scripts/plot_statistics_sector.py
@@ -14,7 +14,7 @@ from _helpers import configure_logging, mock_snakemake
 from add_electricity import sanitize_carriers
 from constants import STATE_2_CODE
 from plot_statistics import create_title
-from summary_sector import (
+from summary_sector import (  # get_load_name_per_sector,
     get_brownfield_capacity_per_state,
     get_capacity_per_node,
     get_emission_timeseries_by_sector,
@@ -26,7 +26,6 @@ from summary_sector import (
     get_historical_transport_consumption_by_mode,
     get_hp_cop,
     get_load_factor_timeseries,
-    get_load_name_per_sector,
     get_load_per_sector_per_fuel,
     get_sector_production_timeseries,
     get_sector_production_timeseries_by_carrier,
@@ -38,7 +37,11 @@ logger = logging.getLogger(__name__)
 
 SECTOR_MAPPER = {
     "res": "residential",
+    "res-rural": "residential-rural",
+    "res-urban": "residential-urban",
     "com": "commercial",
+    "com-rural": "commercial-rural",
+    "com-urban": "commercial-urban",
     "pwr": "power",
     "ind": "industrial",
     "trn": "transport",
@@ -54,6 +57,23 @@ def percent_difference(col_1: pd.Series, col_2: pd.Series) -> pd.Series:
     Calculates percent difference between two columns of numbers.
     """
     return abs(col_1 - col_2).div((col_1 + col_2).div(2)).mul(100)
+
+
+def is_urban_rural_split(n: pypsa.Network) -> bool:
+    """
+    Checks for urban/rural split based on com/res load names.
+    """
+
+    com_res_load = n.loads[
+        (n.loads.index.str.contains("res-")) | (n.loads.index.str.contains("com-"))
+    ].index.to_list()
+
+    rural_urban_loads = ["res-urban-", "res-rural-", "com-urban-", "com-rural-"]
+
+    if any(x in y for x in rural_urban_loads for y in com_res_load):
+        return True
+    else:
+        return False
 
 
 ###
@@ -172,7 +192,7 @@ def plot_hp_cop(n: pypsa.Network, state: Optional[str] = None, **kwargs) -> tupl
             axs[i].set_title(f"{hp}")
 
         except TypeError:  # no numeric data to plot
-            logger.warning(f"No data to plot for {state}")
+            logger.warning(f"No COP data to plot for {state}")
 
     return fig, axs
 
@@ -184,7 +204,7 @@ def plot_sector_production_timeseries(
     **kwargs,
 ) -> tuple:
     """
-    Plots timeseries production.
+    Plots timeseries production as area chart.
     """
 
     investment_period = n.investment_periods[0]
@@ -231,7 +251,7 @@ def plot_sector_production(
     **kwargs,
 ) -> tuple:
     """
-    Plots model period production.
+    Plots model period production as bar chart.
     """
 
     investment_period = n.investment_periods[0]
@@ -385,7 +405,10 @@ def plot_capacity_per_node(
     Plots capacity percentage per node.
     """
 
-    sectors = ("res", "com", "ind")
+    if is_urban_rural_split(n):
+        sectors = ("res-rural", "res-urban", "com-rural", "com-urban", "ind", "trn")
+    else:
+        sectors = ("res", "com", "ind", "trn")
 
     nrows = len(sectors)
 
@@ -437,7 +460,10 @@ def plot_capacity_brownfield(
     Plots old and new capacity at a state level by carrier.
     """
 
-    sectors = ("res", "com", "ind", "trn")
+    if is_urban_rural_split(n):
+        sectors = ("res-rural", "res-urban", "com-rural", "com-urban", "ind", "trn")
+    else:
+        sectors = ("res", "com", "ind", "trn")
 
     nrows = len(sectors)
 
@@ -1351,14 +1377,13 @@ if __name__ == "__main__":
         from _helpers import mock_snakemake
 
         snakemake = mock_snakemake(
-            "plot_system_validate",
-            # "plot_sector_validate",
+            "plot_sector_capacity",
             interconnect="western",
             clusters=100,
             ll="v1.0",
             opts="500SEG",
             sector="E-G",
-            # state="CA",
+            state="AZ",
         )
     configure_logging(snakemake)
 
