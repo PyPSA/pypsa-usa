43b685379b875e46997724b325521794b5cc7657
lfreese <emfreese93@gmail.com>
2025-03-25 14:21:58 -0700

    added the storage state_of_charge rollover, but this is leading to infeasible solve, need to fix

--- workflow/scripts/solve_network.py
+++ workflow/scripts/solve_network.py
@@ -1553,30 +1553,31 @@ def solve_network(n, config, solving, opts="", **kwargs):
                         n.remove(nm, c_idx)
 
                     for df_idx in df.index:
-                        n.madd(
-                            nm,
-                            [df_idx],
-                            carrier=df.loc[df_idx].carrier,
-                            bus=df.loc[df_idx].bus,
-                            p_nom_min=df.loc[df_idx].p_nom_min,
-                            p_nom=df.loc[df_idx].p_nom,
-                            p_nom_max=df.loc[df_idx].p_nom_max,
-                            p_nom_extendable=df.loc[df_idx].p_nom_extendable,
-                            ramp_limit_up=df.loc[df_idx].ramp_limit_up,
-                            ramp_limit_down=df.loc[df_idx].ramp_limit_down,
-                            efficiency=df.loc[df_idx].efficiency,
-                            marginal_cost=df.loc[df_idx].marginal_cost,
-                            capital_cost=df.loc[df_idx].capital_cost,
-                            build_year=df.loc[df_idx].build_year,
-                            lifetime=df.loc[df_idx].lifetime,
-                            heat_rate=df.loc[df_idx].heat_rate,
-                            fuel_cost=df.loc[df_idx].fuel_cost,
-                            vom_cost=df.loc[df_idx].vom_cost,
-                            carrier_base=df.loc[df_idx].carrier_base,
-                            p_min_pu=df.loc[df_idx].p_min_pu,
-                            p_max_pu=df.loc[df_idx].p_max_pu,
-                            land_region=df.loc[df_idx].land_region,
-                        )
+                        n.add(nm, df_idx, **df.loc[df_idx])
+                        # n.madd(
+                        #     nm,
+                        #     [df_idx],
+                        #     carrier=df.loc[df_idx].carrier,
+                        #     bus=df.loc[df_idx].bus,
+                        #     p_nom_min=df.loc[df_idx].p_nom_min,
+                        #     p_nom=df.loc[df_idx].p_nom,
+                        #     p_nom_max=df.loc[df_idx].p_nom_max,
+                        #     p_nom_extendable=df.loc[df_idx].p_nom_extendable,
+                        #     ramp_limit_up=df.loc[df_idx].ramp_limit_up,
+                        #     ramp_limit_down=df.loc[df_idx].ramp_limit_down,
+                        #     efficiency=df.loc[df_idx].efficiency,
+                        #     marginal_cost=df.loc[df_idx].marginal_cost,
+                        #     capital_cost=df.loc[df_idx].capital_cost,
+                        #     build_year=df.loc[df_idx].build_year,
+                        #     lifetime=df.loc[df_idx].lifetime,
+                        #     heat_rate=df.loc[df_idx].heat_rate,
+                        #     fuel_cost=df.loc[df_idx].fuel_cost,
+                        #     vom_cost=df.loc[df_idx].vom_cost,
+                        #     carrier_base=df.loc[df_idx].carrier_base,
+                        #     p_min_pu=df.loc[df_idx].p_min_pu,
+                        #     p_max_pu=df.loc[df_idx].p_max_pu,
+                        #     land_region=df.loc[df_idx].land_region,
+                        # )
                     logger.info(n.consistency_check())
 
                     # copy time-dependent
@@ -1586,6 +1587,14 @@ def solve_network(n, config, solving, opts="", **kwargs):
 
                     for tattr in n.component_attrs[nm].index[selection]:
                         n.import_series_from_dataframe(time_df[tattr], nm, tattr)
+
+                # roll over the sum of time varying storage state of charge to be the state_of_charge_initial
+                # breakpoint()
+                n.storage_units.loc[:, "state_of_charge_initial"] = n.storage_units_t.state_of_charge.loc[
+                    planning_horizon
+                ].sum()
+                # breakpoint()
+
         case _:
             raise ValueError(f"Invalid foresight option: '{foresight}'. Must be 'perfect' or 'myopic'.")
 
