7680df55aa69722ebf20c250acac0212c3adfaad
Henry Daniels-Koch <hdaniels@sh03-ln03.stanford.edu>
2026-02-26 16:46:26 -0800

    run slurm wip

--- workflow/run_slurm.sh
+++ workflow/run_slurm.sh
@@ -89,6 +89,9 @@
 #      --set-resources solve_network:mem_mb=400000
     # --set-resources plot_statistics:mem_mb=10000
 
+
+######################################################
+########### TESTING ##################################
 #Test EGS Western 33 node network
 # snakemake --cluster "sbatch -A {cluster.account} --mail-type ALL --mail-user {cluster.email} -p {cluster.partition} -o {cluster.output} -e {cluster.error} -c {threads} --mem {resources.mem_mb} --time {cluster.walltime}" \
 #     --cluster-config config/config.cluster.yaml \
@@ -96,13 +99,12 @@
 #     --configfile config/config.egs_western.yaml \
 #     --set-resources build_renewable_profiles:mem_mb=40000 
 
-# 33 node network, egs_western_z33_nz2035_perfect
+# 33 node network, egs_western_z33_atlite_nz2035_myopic
 snakemake --cluster "sbatch -A {cluster.account} --mail-type ALL --mail-user {cluster.email} -p {cluster.partition} -o {cluster.output} -e {cluster.error} -c {threads} --mem {resources.mem_mb} --time {cluster.walltime}" \
-    --cluster-config config/260128_config/config.cluster.yaml \
+    --cluster-config config/260225_config/config.cluster.yaml \
     --jobs 40 --latency-wait 60 --rerun-incomplete \
-    --configfile config/260128_config/config.egs_western_z33_nz2035_perfect.yaml \
-    --set-resources build_renewable_profiles:mem_mb=40000,prepare_network:mem_mb=12000 \
-    --set-resources solve_network:mem_mb=300000 
+    --configfile config/260225_config/config.egs_western_z33_atlite_nz2035_myopic.yaml \
+    --set-resources add_electricity:mem_mb=50000 
 
 
 # snakemake --cluster "sbatch -A {cluster.account} --mail-type ALL --mail-user {cluster.email} -p {cluster.partition} -o {cluster.output} -e {cluster.error} -c {threads} --mem {resources.mem_mb} --time {cluster.walltime}" \
